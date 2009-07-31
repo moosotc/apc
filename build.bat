a87e5c3912d48cf9c0947a031d2b8e0b8d46c4d8
malc <av1474@comtv.ru>
2009-08-01 00:21:32 +0400

    Link against user32

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 set libs=unix.cma lablgl.cma lablglut.cma threads.cma
 set flags=-custom -thread -I +lablGL
-ocamlc -o apc.exe %flags% %libs% apc.ml ml_apc.c
+ocamlc -o apc.exe %flags% %libs% apc.ml ml_apc.c -cclib user32.lib
 REM link /edit /subsystem:windows apc.exe
